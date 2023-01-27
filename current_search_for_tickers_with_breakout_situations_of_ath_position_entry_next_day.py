179baa35f115ad156653380b2fae2f077dde2b7b
AlexVijaya <alexopenyoga@yandex.ru>
2023-01-27 18:00:57 +0300

    add # if stock_name!="CAT":             #     continue

--- current_search_for_tickers_with_breakout_situations_of_ath_position_entry_next_day.py
+++ current_search_for_tickers_with_breakout_situations_of_ath_position_entry_next_day.py
@@ -680,8 +680,8 @@ def search_for_tickers_with_breakout_situations(db_where_ohlcv_data_for_stocks_i
             print ( f'{stock_name} is'
                     f' number {counter} out of {len ( list_of_tables_in_ohlcv_db )}\n' )
 
-            if stock_name!="CAT":
-                continue
+            # if stock_name!="CAT":
+            #     continue
 
 
 
